207f2d7bee32b986c444b0e3dbdf7fc178580a0c
denizkavi <denizkavi@outlook.com>
2023-05-19 20:04:27 -0700

    add confirmation

---
+++ send_confirmation.py
@@ -0,0 +1,23 @@
+import argparse
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import (Mail, Attachment, FileContent, FileName, FileType, Disposition)
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('api_key')
+parser.add_argument('email')
+parser.add_argument('sequence')
+
+args = parser.parse_args()
+key = args.api_key
+email = args.email
+sequence = args.sequence
+
+##send confirmation email
+message = Mail(from_email='files@tamarind.bio',
+               to_emails=email,
+               subject="We've recieved your sequence.",
+               html_content='We will get back to you for the results of the structure prediction soon. Thank you for your using Tamarind! Your sequence is:' + sequence)
+
+message.add_cc("files@tamarind.bio")
+

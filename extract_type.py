d0be723089920cdf3313f8f32a76108de404496a
denizkavi <denizkavi@outlook.com>
2023-06-24 15:31:28 -0700

    updates

--- extract_type.py
+++ extract_type.py
@@ -17,13 +17,13 @@ encode = {"Other":0,
         "TAT Lipoprotein signal peptide (Tat/SPII)":4,
         "Pilin-like signal peptide (Sec/SPIII)":5}
 
-for folder in os.listdir("inputs"):
-    if "output.json" in os.listdir(folder):
-        with open(f"inputs/{folder}/output.json") as f:
+for folder in os.listdir("out"):
+    if "output.json" in os.listdir(f"out/{folder}"):
+        with open(f"out/{folder}/output.json") as f:
             j = dict(json.load(f))
 
         res[folder] = j["SEQUENCES"][list(j["SEQUENCES"].keys())[0]]["Prediction"]
-        encoded_res = encode[res[folder]]
+        encoded_res[folder] = encode[res[folder]]
 
     else:
         res[folder] = "?"
